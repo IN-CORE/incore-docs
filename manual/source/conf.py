2932553ae58281eb407583f727f03472d156da63
ncsa-mo <ondrejce@illinois.edu>
2020-06-03 13:14:15 -0500

    changelog, clear notebooks output

--- manual/source/conf.py
+++ manual/source/conf.py
@@ -38,9 +38,9 @@ project = 'IN-CORE Manual'
 author = ''
 
 # The short X.Y version
-version = '1.3.1'
+version = '1.4.0'
 # The full version, including alpha/beta/rc tags
-release = '1.3.1'
+release = '1.4.0'
 
 
 # -- General configuration ---------------------------------------------------
