43aeed03714b0ad50e30b6f40db1de68c62728ec
MO <ondrejce@illinois.edu>
2020-07-30 02:00:25 -0500

    edit config file, edit comments

--- manual/source/conf.py
+++ manual/source/conf.py
@@ -82,8 +82,8 @@ master_doc = 'index'
 # Usually you set "language" from the command line for these cases.
 language = None
 
-# List of patterns, relative to source directory, that match Results and
-# directories to ignore when looking for source Results.
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
@@ -143,8 +143,8 @@ html_theme_options = {
     'titles_only': False
 }
 
-# Add any paths that contain custom static Results (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static Results,
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
@@ -185,7 +185,7 @@ latex_elements = {
     # 'figure_align': 'htbp',
 }
 
-# Grouping the document tree into LaTeX Results. List of tuples
+# Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
@@ -206,10 +206,9 @@ man_pages = [
 # If true, show URL addresses after external links.
 # man_show_urls = False
 
-
 # -- Options for Texinfo output ----------------------------------------------
 
-# Grouping the document tree into Texinfo Results. List of tuples
+# Grouping the document tree into Texinfo files. List of tuples
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
