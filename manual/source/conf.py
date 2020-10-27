56af8b65e8cfbccb3437954cb16d87d550e034a2
MO <ondrejce@illinois.edu>
2020-10-27 12:05:58 -0500

    changelog and version

--- manual/source/conf.py
+++ manual/source/conf.py
@@ -35,9 +35,9 @@ project = 'IN-CORE Manual'
 author = ''
 
 # The short X.Y version
-version = '1.6.0'
+version = '1.7.0'
 # The full version, including alpha/beta/rc tags
-release = '1.6.0'
+release = '1.7.0'
 
 
 # -- General configuration ---------------------------------------------------
