ed7547047457334f282af12b38d40f8bb56008dc
Chris Navarro <cmnavarr@illinois.edu>
2021-05-25 09:03:32 -0500

    INCORE1-1197 added script to push docker images for development, production and branches

--- manual/docker.sh
+++ manual/docker.sh
@@ -9,14 +9,5 @@ export DEBUG=${DEBUG:-""}
 # Find out what branch we are on
 BRANCH=${BRANCH:-"$(git rev-parse --abbrev-ref HEAD)"}
 
-# Find out the version
-if [ "$BRANCH" = "master" ]; then
-    VERSION=""
-elif [ "${BRANCH}" = "develop" ]; then
-    VERSION="-dev"
-else
-    exit 0
-fi
-
 # Build individual images
-$DEBUG docker build -t hub.ncsa.illinois.edu/incore/doc/incore$VERSION:latest .
+$DEBUG docker build -t incore/doc/incore .
