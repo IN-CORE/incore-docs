f6df107772c575e60c0b31ad6e644e58665ecd07
Chen Wang <cwang138@illinois.edu>
2024-02-14 12:05:53 -0600

    fix session 2

--- manual_jb/content/workshops/20231115/session3/wsviz.py
+++ manual_jb/content/workshops/20231115/session3/wsviz.py
@@ -7,7 +7,7 @@ import rasterio
 from rasterio.plot import show_hist
 
 def show_tornado(client, tornado_model_metadata):
-    tornado_dataset_id = tornado_model_metadata['datasetId']
+    tornado_dataset_id = tornado_model_metadata['hazardDatasets'][0]['datasetId']
     tornado_dataset = Dataset.from_data_service(tornado_dataset_id, DataService(client))
     tornado_gdf = gpd.read_file(tornado_dataset.local_file_path)
     map = tornado_gdf.explore(
@@ -26,7 +26,7 @@ def show_tornado_by_id(client, id):
     return show_tornado(client, metadata)
 
 def show_eq_hist(client, eq_metadata):
-    eq_dataset_id=eq_metadata['rasterDataset']['datasetId']
+    eq_dataset_id=eq_metadata['hazardDatasets'][0]['datasetId']
     eq_dataset = Dataset.from_data_service(eq_dataset_id, DataService(client))
     raster_file_path = Path(eq_dataset.local_file_path).joinpath(eq_dataset.metadata['fileDescriptors'][0]['filename'])
     src = rasterio.open(raster_file_path)
